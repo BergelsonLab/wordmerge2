3608b87338046232541f5ec2b15b21eb52c899b9
Gladys Baudet <gladys.baudet@gmail.com>
2019-01-09 16:48:16 -0500

    handle video/single file

--- wordmerge2_annotid.py
+++ wordmerge2_annotid.py
@@ -5,8 +5,23 @@ from shutil import move
 
 ## write functions based on audio or not audio
 
-def create_merged(file_old, file_new, file_merged):
+def create_merged(file_old, file_new, file_merged): #, mode):
     bl_value = "***FIX ME***"
+    """
+    if audio:
+        annotid = "annotid"
+        word = "word"
+        basic_level_col = "basic_level"
+    if video:
+        annotid = "labeled_object.id"
+        word = "labeled_object.object"
+        basic_level_col = "labeled_object.basic_level" # or just basic_level?
+    """
+
+    annotid_col = "annotid"
+    word_col = "word"
+    basic_level_col = "basic_level"
+
     old_error = False
     edit_word = False
     new_word = False
@@ -18,24 +33,24 @@ def create_merged(file_old, file_new, file_merged):
     #df = df.rename(columns={'oldName1': 'newName1'})
     for index, new_row in new_df.iterrows():
 
-        word = ''
+        #word = ''
         to_add = new_row
-        id = new_row['annotid']
-        tmp = old_df[old_df['annotid']==id]
+        id = new_row[annotid_col]
+        tmp = old_df[old_df[annotid_col]==id]
         # print(len(tmp.index))
 
-        word = new_row['word']
-        tier = new_row['tier']
-        spk = new_row['speaker']
-        utt_type = new_row['utterance_type']
-        obj_pres = new_row['object_present']
-        ts = new_row['timestamp']
+        word = new_row[word_col]
+        # tier = new_row['tier']
+        # spk = new_row['speaker']
+        # utt_type = new_row['utterance_type']
+        # obj_pres = new_row['object_present']
+        # ts = new_row['timestamp']
 
         while len(tmp.index) != 0: # if the id already exists in the old df, check that the words/ts? do match
 
             if len(tmp.index) > 1:
                 print("ERROR: annotid not unique in old version : ", id) # raise exception
-                to_add['basic_level'] = bl_value
+                to_add[basic_level_col] = bl_value
                 merged_df = merged_df.append(to_add)
                 old_error = True
                 break
@@ -44,15 +59,15 @@ def create_merged(file_old, file_new, file_merged):
 
 
             # if new_row[:, new_row.columns != "basic_level"].equals(old_row[:, old_row.columns != "basic_level"]):
-            if word == old_row['word']:
+            if word == old_row[word_col]:
                 # print("old", word)
                 # check codes as well to know if something changed?
-                to_add['basic_level'] = old_row['basic_level']
+                to_add[basic_level_col] = old_row[basic_level_col]
                 merged_df = merged_df.append(to_add)
                 break
             else:
                 # print("old but different", word)
-                to_add['basic_level'] = bl_value
+                to_add[basic_level_col] = bl_value
                 merged_df = merged_df.append(to_add)
                 edit_word = True
                 break
@@ -60,7 +75,7 @@ def create_merged(file_old, file_new, file_merged):
         else: # if the id is new: no info to retrieve, add row from new
             if word != '':
                 # print("new", word)
-                to_add['basic_level'] = bl_value
+                to_add[basic_level_col] = bl_value
                 merged_df = merged_df.append(to_add)
                 new_word = True
                 break
@@ -72,8 +87,6 @@ def create_merged(file_old, file_new, file_merged):
 
 if __name__ == "__main__":
 
-    home_visit_paths = sys.argv[1]
-
     old_error_list = []
     edit_word_list = []
     new_word_list = []
@@ -81,45 +94,67 @@ if __name__ == "__main__":
     today = str(datetime.datetime.now().year)+"_" \
             + str(datetime.datetime.now().month)+"_" \
             + str(datetime.datetime.now().day)+"_"
-    with open(home_visit_paths, 'r') as f:
-        lines = f.readlines()
 
-    for line in lines:
-        print(line)
-        line = line.strip()
-
-        # get name of old .csv file (with bl)+path to merged
-        old_path = line+"/Analysis/Audio_Analysis/"
-        for csv_file in os.listdir(old_path):
-            if csv_file.endswith("audio_sparse_code.csv"):
-                move(os.path.join(old_path, csv_file), os.path.join(old_path, "old_files", today+csv_file))
-                old = os.path.join(old_path, "old_files", today+csv_file)
-                out = os.path.join(old_path, csv_file)
-
-        # get name of new .csv file (no bl)
-        new_path = line+"/Coding/Audio_Annotation/"
-        for csv_file in os.listdir(new_path):
-            if csv_file.endswith("processed.csv"):
-                new = os.path.join(new_path, csv_file)
-
-        # compute merge
+    # if only one file
+    if len(sys.argv) >= 4:
+        old = sys.argv[1]
+        new = sys.argv[2]
+        out = sys.argv[3]
+        if len(sys.argv)>=5:
+            mode = sys.argv[4]
+            
         if old and out and new:
             old_error, edit_word, new_word = create_merged(old, new, out)
-            if old_error:
-                old_error_list.append(line)
-            if edit_word:
-                edit_word_list.append(line)
-            if new_word:
-                new_word_list.append(line)
 
-    # at the very end, write every error/change encountered
-    with open("annotid_merge_errors.txt", "w+") as f:
-        f.write("old_errors\n")
-        for l in old_error_list:
-            f.write(l+"\n")
-        f.write("edit_word\n")
-        for l in edit_word_list:
-            f.write(l+"\n")
-        f.write("new_word\n")
-        for l in new_word_list:
-            f.write(l+"\n")
+        """
+        Do everything needed if only file by file
+        """
+
+    # if all files ## <4 because mode could be added
+    if len(sys.argv) >= 2 and len(sys.argv)<4:
+        home_visit_paths = sys.argv[1]
+        if len(sys.argv) > 2:
+            mode = sys.argv[2]
+
+        with open(home_visit_paths, 'r') as f:
+            lines = f.readlines()
+
+        for line in lines:
+            print(line)
+            line = line.strip()
+
+            # get name of old .csv file (with bl)+path to merged
+            old_path = line+"/Analysis/Audio_Analysis/"
+            for csv_file in os.listdir(old_path):
+                if csv_file.endswith("audio_sparse_code.csv"):
+                    move(os.path.join(old_path, csv_file), os.path.join(old_path, "old_files", today+csv_file))
+                    old = os.path.join(old_path, "old_files", today+csv_file)
+                    out = os.path.join(old_path, csv_file)
+
+            # get name of new .csv file (no bl)
+            new_path = line+"/Coding/Audio_Annotation/"
+            for csv_file in os.listdir(new_path):
+                if csv_file.endswith("processed.csv"):
+                    new = os.path.join(new_path, csv_file)
+
+            # compute merge
+            if old and out and new:
+                old_error, edit_word, new_word = create_merged(old, new, out)
+                if old_error:
+                    old_error_list.append(line)
+                if edit_word:
+                    edit_word_list.append(line)
+                if new_word:
+                    new_word_list.append(line)
+
+        # at the very end, write every error/change encountered
+        with open("annotid_merge_errors.txt", "w+") as f:
+            f.write("old_errors\n")
+            for l in old_error_list:
+                f.write(l+"\n")
+            f.write("edit_word\n")
+            for l in edit_word_list:
+                f.write(l+"\n")
+            f.write("new_word\n")
+            for l in new_word_list:
+                f.write(l+"\n")
